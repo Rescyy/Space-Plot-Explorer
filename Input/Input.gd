83788737295dcec5a6b71f19c8dc1e7a10bfee85
Rescyy <vcrucerescu6@gamil.com>
2024-10-29 00:09:00 +0200

    Connected Input to Spaceship through global listenable

--- Input/Input.gd
+++ Input/Input.gd
@@ -1,15 +1,10 @@
 extends Node
 
-@onready var inputRef: CharacterBody2D = $"../../Spaceship"
-
 var x = 0.0
 var y = 0.0
 var math_expr = ""
 var time_passed = 0.0
 var positions = []
-
-func _ready() -> void:
-	inputRef.connect("text_entered", Callable(self, "_on_func_submit"))
 
 func _process(delta: float) -> void:
 	pass
@@ -20,7 +15,9 @@ func _on_text_submitted(p_expr: String) -> void:
 	x = 0.0
 	time_passed = 0.0
 	positions.clear()
-	
+	var user_func_callable = Callable(self, "user_func")
+	Global.set_user_func(user_func_callable)
+
 func evaluate(command: String, variable_names: Array, variable_values: Array) -> float:
 	var expression = Expression.new()
 	var error = expression.parse(command, variable_names)
--- Spaceship/spaceship.gd
+++ Spaceship/spaceship.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 var relative_position = Vector2(0, 0)
@@ -9,21 +9,27 @@ var end_x = 1000
 var func_trajectory: Array[Vector2] = [Vector2.ZERO]
 var trajectory_distances: Array[float] = [0]
 var distance: float = 0
-var is_moving = true
-
-func user_func(x: float) -> float:
-	var y = x
-	return y
+var _user_func: Callable
+var is_moving = false
 
 func _ready() -> void:
+	self.init_pos = position
+
+func compute_trajectory():
 	var x: int = 0
-	init_pos = position
 	while x < end_x:
 		x += 1
-		func_trajectory.append(Vector2(x, user_func(x)))
+		func_trajectory.append(Vector2(x, _user_func.call(x)))
 		trajectory_distances.append(func_trajectory[x - 1].distance_to(func_trajectory[x - 2]) + trajectory_distances.back())
 
+func start(user_func: Callable):
+	_user_func = user_func
+	compute_trajectory()
+	is_moving = true
+
 func _physics_process(delta: float) -> void:
+	if Global.has_user_func and not is_moving:
+		start(Global.user_func)
 	if is_moving:
 		var ds = delta * SPEED
 		distance += ds
@@ -32,8 +38,8 @@ func _physics_process(delta: float) -> void:
 		var d2 = trajectory_distances[i]
 		var t = (distance - d1) / (d2 - d1)
 		var x = i + t - 1
-		var y = user_func(x)
-		var new_position = init_pos + Vector2(x, -y)
+		var y = _user_func.call(x)
+		var new_position = self.init_pos + Vector2(x, -y)
 		var delta_position = new_position - position
 		rotation = -Vector2(-delta_position.y, -delta_position.x).angle()
 		position = new_position
