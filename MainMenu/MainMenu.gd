0788cf32a953fba7dc02050d016bc8f6f36ffce3
malmilo6 <maxim.cernetchi@isa.utm.md>
2024-11-27 23:58:51 +0200

    feat: configure level selector; add buttons; link buttons to scenes; adjust lost game logic;

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -12,4 +12,5 @@ func _on_quit_button_pressed():
 func _on_play_button_pressed() -> void:
 	State.set_idle()
 	#get_tree().change_scene_to_file("res://Levels/Level1/level.tscn")
-	get_tree().change_scene_to_file("res://ProceduralLevel/proceduralLevel.tscn")
+	#get_tree().change_scene_to_file("res://ProceduralLevel/proceduralLevel.tscn")
+	get_tree().change_scene_to_file("res://LevelSelector/level_selector.tscn")
