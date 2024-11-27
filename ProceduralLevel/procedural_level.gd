8f0aeeb003594c95be3956fbc5487a53d1cadf3c
Crucerescu Vladislav <crucerescuvladislav@Crucerescus-MacBook-Air.local>
2024-11-27 20:46:12 +0200

    Initialize Prcedural Node

---
+++ ProceduralLevel/procedural_level.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var size: Vector2 = get_viewport_rect().size
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
